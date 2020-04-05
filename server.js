07db3bba3b71c68e3df2ccc860a39b53e7534984
jlam <jennylamwork@gmail.com>
2020-04-05 01:58:14 -0700

    added middleware

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
